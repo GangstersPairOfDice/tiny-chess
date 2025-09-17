a1d00884b6a22939956e03761d0f5013113a749f
GangsterPairOfDice <daniel.lyakhovich@gmail.com>
2025-09-17 18:30:55 +0200

    added test game

--- test.py
+++ test.py
@@ -1,5 +1,20 @@
-import chess
+import chess.pgn
 
-board = chess.Board()
+pgn = open("test.pgn")
+
+game = chess.pgn.read_game(pgn)
+
+# iter through all moves and play them on a board.
+
+board = game.board()
 
 print(board)
+print('')
+
+for move in game.mainline_moves():
+  board.push(move)
+  print(board)
+  print('')
+
+board
+print(board.is_checkmate())
